fb0833cdca9fb1abf00fd48d388681f0940f85c8
Jackson Romie <jromie0924@gmail.com>
2021-01-18 11:20:23 -0700

    Stub rabbitmq connection methods

---
+++ main.py
@@ -0,0 +1,25 @@
+import pika
+
+username = ""
+
+
+def establishConnection():
+    global connection
+    global channel
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters("localhost"))
+    channel = connection.channel()
+
+
+def createSession():
+    channel.exchange_declare("conversation", exchange_type="direct")
+    channel.queue_declare(queue=username)
+    channel.queue_bind(
+        queue=username, exchange="conversation", routing_key=username)
+
+
+if __name__ == "__main__":
+    print("Enter username")
+    username = input()
+    establishConnection()
+    createSession()
