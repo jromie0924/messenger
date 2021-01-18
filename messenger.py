bb03be0b24647ca47e2b8e5676d3f8cf836335ca
Jackson Romie <jromie0924@gmail.com>
2021-01-18 16:37:42 -0700

    Put receiver in multiprocess, and add processor

--- messenger.py
+++ messenger.py
@@ -2,6 +2,8 @@ import multiprocessing
 import pika
 from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
 from pika.spec import Basic, BasicProperties
+import time
+import sys
 
 
 class receiver():
@@ -11,11 +13,10 @@ class receiver():
     queue = ""
 
     def __init__(self, url, exchange, username):
-        print("receiver init")
         self.connection = self.open_connection(url)
         self.exchange = exchange
-        self.setup_channel()
         self.queue = username
+        self.setup_channel()
         try:
             self._channel.start_consuming()
         except KeyboardInterrupt:
@@ -38,21 +39,54 @@ class receiver():
     def on_message(self, ch, deliveryArgs: Basic.Deliver, properties: BasicProperties, body: bytes):
         message = body.decode()
         sender = properties.headers.get("sender")
-        print("FROM " + sender + " :: " + message)
+        print("FROM " + sender + " :: " + message + "\n")
         self._channel.basic_ack(delivery_tag=deliveryArgs.delivery_tag)
 
 
 class publisher(object):
-    def __init__(self, url, exchange):
-        print("publisher init")
+    _connection: BlockingConnection = None
+    _channel: BlockingChannel = None
+    _exchange = ""
+    _routing_key = ""
+    _username = ""
+
+    def __init__(self, url, exchange, friend, username):
+        self._connection = self.open_connection(url)
+        self._exchange = exchange
+        self._routing_key = friend
+        self._username = username
+        self._channel = self._connection.channel()
+        self.prompt_messages()
+
+    def open_connection(self, url):
+        return pika.BlockingConnection(parameters=pika.URLParameters(url))
+
+    def prompt_messages(self):
+        while True:
+            message = input("> ")
+            self.publish_message(message)
+
+    def publish_message(self, message):
+        self._channel.basic_publish(self._exchange, routing_key=self._routing_key, body=message,
+                                    properties=pika.BasicProperties(headers={"sender": self._username}))
 
 
 def main():
     url = "amqp://guest:guest@localhost:5672"
     exchange = "conversation"
-    username = input("Enter username\n")
-    r = receiver(url, exchange)
-    p = publisher(url, exchange)
+    username = input("Enter username: ")
+    print("\n\n")
+    friend = ""
+    if username == "jackson":
+        friend = "taumer"
+    elif username == "taumer":
+        friend = "jackson"
+
+    _receiver = multiprocessing.Process(
+        target=receiver, args=(url, exchange, username))
+    _receiver.start()
+
+    _publisher = publisher(url, exchange, friend, username)
 
 
 if __name__ == "__main__":
