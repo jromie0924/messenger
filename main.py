fdce5f87fb08546650fe402e69967d9c2e7afdc5
Jackson Romie <jromie0924@gmail.com>
2021-01-18 11:51:11 -0700

    add listening logic for session

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pika
+# from pika.spec import BasicProperties
 
-username = ""
+connection = None
+channel = None
+username = None
 
 
 def establishConnection():
@@ -13,8 +16,11 @@ def establishConnection():
 
 def createSession():
     channel.exchange_declare("sessions", exchange_type="fanout")
-    channel.queue_declare(queue="{username}_session")
-    channel.queue_bind(queue="{username}_session", exchange="sessions")
+    channel.queue_declare(queue=username + "_session")
+    channel.queue_bind(queue=username + "_session", exchange="sessions")
+
+    channel.basic_publish(exchange="sessions",
+                          routing_key="newSession", body=username)
 
     channel.exchange_declare("conversation", exchange_type="direct")
     channel.queue_declare(queue=username)
@@ -22,8 +28,26 @@ def createSession():
         queue=username, exchange="conversation", routing_key=username)
 
 
+def listen():
+    channel.basic_consume(queue=username + "_session",
+                          on_message_callback=sessionCallback)
+    channel.start_consuming()
+
+
+def sessionCallback(ch, method, properties, body):
+    body = body.decode()
+    if body != username:
+        print(" [x] %r has entered the chat" % body)
+        channel.basic_ack(method.delivery_tag, multiple=True)
+
+
+def chatCallback(ch, method, properties, body):
+    print(body)
+
+
 if __name__ == "__main__":
     print("Enter username")
     username = input()
     establishConnection()
     createSession()
+    listen()
