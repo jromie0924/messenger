ac3a521e2527990f47eeff43f9026e0646f9422c
Jackson Romie <jromie0924@gmail.com>
2021-01-18 22:10:53 -0700

    removal of session logic

--- messenger.py
+++ messenger.py
@@ -1,6 +1,5 @@
 import pika
 import multiprocessing
-import sys
 from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
 from pika.spec import Basic, BasicProperties
 
@@ -24,11 +23,7 @@ class receiver():
         self._channel.start_consuming()
 
     def open_connection(self, url):
-        creds = pika.PlainCredentials("guest", "guest")
-        params = pika.ConnectionParameters(
-            heartbeat=600, host="localhost", credentials=creds)
-        # return pika.BlockingConnection(parameters=pika.URLParameters(url))
-        return pika.BlockingConnection(parameters=params)
+        return pika.BlockingConnection(pika.URLParameters(url))
 
     def setup_channel(self):
         self._channel = self.connection.channel()
@@ -50,18 +45,11 @@ class receiver():
     def on_message(self, ch: BlockingChannel, deliveryArgs: Basic.Deliver, properties: BasicProperties, body: bytes):
         message = body.decode()
 
-        type = properties.headers.get("type")
-        if type is not None and type == SESSION_START_TYPE:
-            # name = body.decode()
-            # print("[x]\t" + name + " has joined the session.\n")
-            ch.basic_ack(delivery_tag=deliveryArgs.delivery_tag, multiple=True)
-
-        else:
-            sender = properties.headers.get("sender")
-            if sender != self.queue:  # username
-                print("FROM " + sender + " :: " + message + "\n")
-                ch.basic_ack(
-                    delivery_tag=deliveryArgs.delivery_tag, multiple=True)
+        sender = properties.headers.get("sender")
+        if sender != self.queue:  # username
+            print("FROM " + sender + " :: " + message + "\n")
+            ch.basic_ack(
+                delivery_tag=deliveryArgs.delivery_tag, multiple=True)
 
 
 class publisher():
@@ -77,7 +65,6 @@ class publisher():
         self._routing_key = username
         self._username = username
         self._channel = self._connection.channel()
-        self.publish_session()
         self.prompt_messages()
 
     def open_connection(self, url):
@@ -91,10 +78,6 @@ class publisher():
     def publish_message(self, message):
         self._channel.basic_publish(self._exchange, routing_key="message", body=message,
                                     properties=pika.BasicProperties(headers={"sender": self._username, "type": MESSAGE_TYPE}))
-
-    def publish_session(self):
-        self._channel.basic_publish(exchange=SESSION_EXCHANGE, routing_key=self._username + "_session",
-                                    body=self._username, properties=pika.BasicProperties(headers={"type": SESSION_START_TYPE}))
 
 
 def main():
