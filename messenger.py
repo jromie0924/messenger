4379cc62f2bd7b2fafff849a40c1164409135a14
Jackson Romie <jromie0924@gmail.com>
2021-01-18 21:57:43 -0700

    new exchange for tracking sessions

--- messenger.py
+++ messenger.py
@@ -4,6 +4,11 @@ import sys
 from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
 from pika.spec import Basic, BasicProperties
 
+SESSION_EXCHANGE = "sessions"
+SESSION_START_TYPE = "session-start"
+MESSAGE_TYPE = "message"
+FRIEND = ""
+
 
 class receiver():
     _channel: BlockingChannel = None
@@ -19,24 +24,44 @@ class receiver():
         self._channel.start_consuming()
 
     def open_connection(self, url):
-        return pika.BlockingConnection(parameters=pika.URLParameters(url))
+        creds = pika.PlainCredentials("guest", "guest")
+        params = pika.ConnectionParameters(
+            heartbeat=600, host="localhost", credentials=creds)
+        # return pika.BlockingConnection(parameters=pika.URLParameters(url))
+        return pika.BlockingConnection(parameters=params)
 
     def setup_channel(self):
         self._channel = self.connection.channel()
         self._channel.exchange_declare(
-            exchange=self.exchange, exchange_type="direct")
+            exchange=self.exchange, exchange_type="fanout")
+
+        self._channel.exchange_declare(
+            exchange=SESSION_EXCHANGE, exchange_type="fanout")
+
         self._channel.queue_declare(queue=self.queue)
+
         self._channel.queue_bind(
             queue=self.queue, exchange=self.exchange, routing_key=self.queue)
+        self._channel.queue_bind(queue=self.queue, exchange=SESSION_EXCHANGE)
 
         self._channel.basic_consume(
             queue=self.queue, on_message_callback=self.on_message)
 
-    def on_message(self, ch, deliveryArgs: Basic.Deliver, properties: BasicProperties, body: bytes):
+    def on_message(self, ch: BlockingChannel, deliveryArgs: Basic.Deliver, properties: BasicProperties, body: bytes):
         message = body.decode()
-        sender = properties.headers.get("sender")
-        print("FROM " + sender + " :: " + message + "\n")
-        self._channel.basic_ack(delivery_tag=deliveryArgs.delivery_tag)
+
+        type = properties.headers.get("type")
+        if type is not None and type == SESSION_START_TYPE:
+            # name = body.decode()
+            # print("[x]\t" + name + " has joined the session.\n")
+            ch.basic_ack(delivery_tag=deliveryArgs.delivery_tag, multiple=True)
+
+        else:
+            sender = properties.headers.get("sender")
+            if sender != self.queue:  # username
+                print("FROM " + sender + " :: " + message + "\n")
+                ch.basic_ack(
+                    delivery_tag=deliveryArgs.delivery_tag, multiple=True)
 
 
 class publisher():
@@ -49,9 +74,10 @@ class publisher():
     def __init__(self, url, exchange, friend, username):
         self._connection = self.open_connection(url)
         self._exchange = exchange
-        self._routing_key = friend
+        self._routing_key = username
         self._username = username
         self._channel = self._connection.channel()
+        self.publish_session()
         self.prompt_messages()
 
     def open_connection(self, url):
@@ -63,8 +89,12 @@ class publisher():
             self.publish_message(message)
 
     def publish_message(self, message):
-        self._channel.basic_publish(self._exchange, routing_key=self._routing_key, body=message,
-                                    properties=pika.BasicProperties(headers={"sender": self._username}))
+        self._channel.basic_publish(self._exchange, routing_key="message", body=message,
+                                    properties=pika.BasicProperties(headers={"sender": self._username, "type": MESSAGE_TYPE}))
+
+    def publish_session(self):
+        self._channel.basic_publish(exchange=SESSION_EXCHANGE, routing_key=self._username + "_session",
+                                    body=self._username, properties=pika.BasicProperties(headers={"type": SESSION_START_TYPE}))
 
 
 def main():
@@ -73,10 +103,6 @@ def main():
     username = input("Enter username: ")
     print("\n--------------------\n")
     friend = ""
-    if username == "jackson":
-        friend = "taumer"
-    elif username == "taumer":
-        friend = "jackson"
 
     _receiver = multiprocessing.Process(
         target=receiver, args=(url, exchange, username))
