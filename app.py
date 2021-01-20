9c1a64e741b4d37a3f84fe1e779d2f26903dbb84
Jackson Romie <jromie0924@gmail.com>
2021-01-20 08:46:39 -0700

    More code cleanup

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request, session
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
 from messenger import receiver, publisher
-import multiprocessing
-from threading import Thread, Event
-from flask_socketio import SocketIO, emit
+from threading import Thread
+from flask_socketio import SocketIO
 import uuid
 
 app = Flask(__name__)
@@ -12,10 +11,7 @@ app.secret_key = "my secret key"
 
 socketio = SocketIO(app)
 
-messageList = []
 web_app = None
-receiver_process = None
-logged_in = ""
 url = "amqp://guest:guest@localhost:5672"
 exchange = "conversation"
 thread: Thread = None
@@ -26,12 +22,9 @@ web_apps = {}
 # Function to be passed to the receiver class as its message handler.
 def on_message_handler(ch: BlockingChannel, deliveryArgs: Basic.Deliver, properties: BasicProperties, body: bytes):
     message = body.decode()
-    print(deliveryArgs)
-    print(ch)
+
     # It is assumed that every message will have a "sender" header associated with the sender's username.
     sender = properties.headers.get("sender")
-    print(sender)
-    print(f"EMMITING MESSAGE {logged_in}:{message}")
     message = f"{sender}: {message}"
     socketio.emit("newMessage", {"message": message})
     ch.basic_ack(
@@ -41,10 +34,8 @@ def on_message_handler(ch: BlockingChannel, deliveryArgs: Basic.Deliver, propert
 # webapp class
 class webapp():
     _publisher: publisher = None
-    _username = ""
 
     def setup(self, username):
-        self._username = username
         self._publisher = publisher(url, exchange, username)
 
         def start():
@@ -58,34 +49,31 @@ class webapp():
         self._publisher.publish_message(message)
 
 
+# Website user entry point
 @app.route('/', methods=["GET"])
 def index():
-    return render_template("index.html", messages=messageList)
+    return render_template("index.html")
 
 
+# Capture username and instantiate a webapp instance.
 @app.route('/username', methods=["POST", "GET"])
 def get_username():
     form_data = request.form
     username = form_data.getlist("username")[0]
     session['username'] = username
-    print(session)
     web_app = webapp()
     web_app.setup(username)
     web_apps[session['username']] = web_app
-    global logged_in
-    logged_in = username
-    print("LOGGED IN AS " + username)
     return render_template("index.html", hasUsername=1, username=username)
 
 
+# Captures messages received from AMQP
 @socketio.on('MESSAGE')
 def my_test(data):
-    print(len(threads))
-    print(data)
     try:
         web_apps.get(session['username']).publish_message(data)
     except:
-        print("cant find that user!")
+        print("User cannot be found.")
 
 
 if __name__ == "__main__":
